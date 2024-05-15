00afd19adcf10a5700a834f07a6116e5a35d5c26
realhardik18 <realhardik18@gmail.com>
2024-05-15 14:03:35 +0530

    working on scorecard logic

--- src/Scorecard.jsx
+++ src/Scorecard.jsx
@@ -1,11 +1,15 @@
-import React, { useState } from "react";
+import { useState } from "react";
+import { hello } from "./scorecard.js"
 
 function Scorecard(){
     const [score,runs]=useState(0);
     const [balls,faced]=useState(0);
+    var decmial = balls%6
+    var overs= `${Math.floor(balls/6)}.${decmial}`
     return(
         <div>
-            <h1>{score} off {balls}</h1>
+            <h1>{score} off {overs}</h1>
+            <button className="btn btn-active btn-accent" onClick={hello}>say hi</button>
             <button className="btn btn-active btn-accent" onClick={()=>{runs(score + 1); faced(balls+1)}}>single</button>
             <button className="btn btn-active btn-accent" onClick={()=>{runs(score + 2); faced(balls+1)}}>double</button>
             <button className="btn btn-active btn-accent" onClick={()=>{runs(score + 3); faced(balls+1)}}>triple</button>
