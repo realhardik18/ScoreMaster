dd5ffa76d8cccafee510b39cd6638d226b495f3a
realhardik18 <realhardik18@gmail.com>
2024-11-15 13:18:18 +0530

    swtiching to react native

---
+++ app/home.tsx
@@ -0,0 +1,10 @@
+import { SafeAreaView } from "react-native-safe-area-context";
+import { Text } from "react-native";
+
+export default function home(){
+    return(
+        <SafeAreaView>
+            <Text>welcome home</Text>
+        </SafeAreaView>
+    )
+}
